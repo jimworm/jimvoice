7e9c634437ae22ccac8fc51914c198d1e4a7fbde
Pete H <pete@onapp.com>
2012-07-05 01:05:52 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jimvoice::Application.config.secret_token = 'a6bb54e96d117a3c02f03896d85cca67c749832f4fe1d507bcf649f7fbe3392b83d587383ae67245351db889e2adaaf3e801bb9175dfae247dc6d46e8fdc8b3a'
