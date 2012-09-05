78483530d600705ad0de388d53b51ecd69ff786b
Pete <jimworm@gmail.com>
2012-09-05 12:04:44 +0200

    typo in mailer settings

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address:               'smtp.gmail.com',
   port:                  587,
   domain:                'gmail.com',
-  username:              ENV['EMAIL_USERNAME'],
+  user_name:             ENV['EMAIL_USERNAME'],
   password:              ENV['EMAIL_PASSWORD'],
   authentication:        'plain',
   enable_starttls_auto:  true
