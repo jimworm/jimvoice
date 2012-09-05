8115114264baf327a4620cfa6ab89ed73b70c3be
Pete <jimworm@gmail.com>
2012-09-05 12:38:19 +0200

    more explicit email subject

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -5,6 +5,6 @@ class InvoiceMailer < ActionMailer::Base
     fail 'Only saved invoices can be issued' unless invoice.persisted?
     @invoice = invoice
     @client = invoice.client
-    mail(:to => "#{@client.name} <#{@client.email}>", :subject => "Invoice #{@invoice.id}")
+    mail(:to => "#{@client.name} <#{@client.email}>", :subject => "Jim's Jams and Marmalades Invoice #{@invoice.id}")
   end
 end
