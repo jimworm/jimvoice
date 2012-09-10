7a332a6e71a6a709d1e29b80000f974cb76715d0
Pete <jimworm@gmail.com>
2012-09-10 23:18:26 +0200

    changed invoice cc to bcc

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -4,6 +4,6 @@ class InvoiceMailer < ActionMailer::Base
   def issue(invoice)
     @invoice = invoice
     @client = invoice.client
-    mail(to: "#{@client.name} <#{@client.email}>", cc: "Robert Hau <robert@jimsjamsmarmalades.com>", subject: "#{'[TEST] ' unless Rails.env.production?}Jim's Jams and Marmalades Invoice #{@invoice.id}")
+    mail(to: "#{@client.name} <#{@client.email}>", bcc: "Robert Hau <robert@jimsjamsmarmalades.com>", subject: "#{'[TEST] ' unless Rails.env.production?}Jim's Jams and Marmalades Invoice #{@invoice.id}")
   end
 end
